c8802499039a3ee3d1c513ac81066c200003d2c7
Astrian Zheng <git@astrianzheng.cn>
2022-01-27 21:47:50 +0800

    Verify token

--- func/verify-captcha.js
+++ func/verify-captcha.js
@@ -3,9 +3,11 @@ const { verify } = require('hcaptcha')
 module.exports = async (token) => {
   try {
     let data = await verify("0x96Aa8DAd3095f4ff9548d47561b4c8533dd2F9E5", token)
-    
+    if (!data.success) {
+      throw new Error(JSON.stringify({code: 400, message: '未能通过人机验证'}))
+    }
   } catch(e) {
     console.log(e)
-    throw new Error(JSON.stringify({code: 400, message: 'hCaptcha 服务器出现错误'}))
+    throw new Error(JSON.stringify(e.message).code || JSON.stringify({code: 400, message: 'hCaptcha 服务器出现错误'}))
   }
 }
