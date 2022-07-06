2e9e23f7d19b8f05a4aa65a1dc21c751f4d5bc55
Astrian Zheng <red.dusk5535@astrianzheng.com>
2022-07-06 01:41:29 +0000

    Set fallback locale

--- frontend/src/i18n.js
+++ frontend/src/i18n.js
@@ -19,6 +19,6 @@ function loadLocaleMessages () {
 
 export default new VueI18n({
   locale: process.env.VUE_APP_I18N_LOCALE || navigator.language,
-  fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || navigator.language,
+  fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || 'en',
   messages: loadLocaleMessages()
 })
