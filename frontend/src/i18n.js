8726a58881b81608c4695665303dd9ab2017232a
Astrian Zheng <happy.wheel6743@astrian.moe>
2022-09-18 15:35:10 +1000

    change locazation file

--- frontend/src/i18n.js
+++ frontend/src/i18n.js
@@ -5,7 +5,7 @@ Vue.use(VueI18n)
 
 function loadLocaleMessages () {
   console.log(navigator.language)
-  const locales = require.context('./locales', true, /[A-Za-z0-9-_,\s]+\.json$/i)
+  const locales = require.context('./locales', true, /[A-Za-z0-9-_,\s]+\/strings\.json$/i)
   const messages = {}
   locales.keys().forEach(key => {
     const matched = key.match(/([A-Za-z0-9-_]+)\./i)
