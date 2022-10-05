445430d41a5485db7359e0058eb205fb3dbd5391
Astrian Zheng <red.dusk5535@astrianzheng.com>
2022-10-05 02:52:41 +0000

    Initial vue 3 project

---
+++ frontend-vue3/src/App.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+import { RouterLink } from 'vue-router'
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    RouterLink
+  }
+})
+</script>
+
+<template>
+  <span>hello</span>
+</template>
+
+<style scoped>
+
+</style>
